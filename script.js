f20e0cba8242093d079cb22baef7edee90564b20
hktari <bkamnik1995@gmail.com>
2022-12-06 18:44:50 +0100

    adjust flip card layout on mobile

--- script.js
+++ script.js
@@ -55,19 +55,3 @@ function flipCardClickEventHandler(ev) {
 function toggleFlipCardDetails(flipCard, forceShowDetails = undefined) {
     flipCard.classList.toggle('c-flip-card--show-details', forceShowDetails)
 }
-
-
-
-
-
-// This will log the width of the viewport
-console.log(window.innerWidth);
-
-// This will log the width of the frame viewport within a frameset
-console.log(self.innerWidth);
-
-// This will log the width of the viewport of the closest frameset
-console.log(parent.innerWidth);
-
-// This will log the width of the viewport of the outermost frameset
-console.log(top.innerWidth);
