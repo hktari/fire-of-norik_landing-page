2ab3916cb90baa59fc678d7a14facbe61d512505
hktari <bkamnik1995@gmail.com>
2022-12-06 11:15:34 +0100

    setup loading indicator logic

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-const bgVideo = document.getElementById('bgVideo')
-bgVideo.
+const videoList = document.getElementsByTagName('video')
+const audioList = document.getElementsByTagName('audio')
+const loadingInd = document.getElementById('loadingIndicator')
+const page = document.getElementById('pageRoot')
+
+window.addEventListener("load", (event) => {
+    console.log('loaded !')
+    loadingInd.style.display = 'none';
+
+    for (const audio of audioList) {
+        audio.autoplay = true;
+    }
+
+    for (const video of videoList) {
+        video.autoplay = true;
+    }
+
+
+    page.classList.remove('c-page--no-scroll');
+});
+
+console.log('done')
---
+++ simulate-loading.js
@@ -0,0 +1,6 @@
+const itemCount = 1000;
+for (let i = 0; i < itemCount; i++) {
+    const img = document.createElement('img')
+    img.src = '/images/jonathan-kemper-hs1-YdfShbg-unsplash.jpg'
+    document.body.appendChild(img)
+}
